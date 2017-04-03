e153b201cba54017b768905c2aed1a373feac649
Carlos Vidal <nakioparkour@gmail.com>
2017-04-03 20:07:22 +0100

    disabled webrick logging

--- lib/joaquin/node.rb
+++ lib/joaquin/node.rb
@@ -1,30 +1,45 @@
 require 'rack'
+require 'webrick'
+require 'json'
 
 module Joaquin
   class Node
 
-    def self.call(env)
-      require 'pp'
-      pp env
-      [200, {'Content-Type' => 'text/html'}, ['derp']]
+    @@jobs = {}
+
+    def self.call(request)
+      # Status
+      # Run job
+      # Cancel job
+      # Job details
+      [200, {'Content-Type' => 'application/json'}, [request.to_json]]
     end
 
     def self.start(options)
-      # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/webrick/rdoc/WEBrick.html
       rack_options = {
         Host: '127.0.0.1',
-        Port: options.port
+        Port: options.port,
+        Logger: WEBrick::Log.new("/dev/null"),
+        AccessLog: []
       }
 
       Print.warning("Booting up node at #{rack_options[:Host]}:#{rack_options[:Port]}...")
       Rack::Handler::WEBrick.run(Node, rack_options) do |server|
-        [:INT, :TERM].each do |signal|
-          trap(signal) do
+        Print.success("Node running at #{server.config[:BindAddress]}:#{server.config[:Port]}")
+        [:INT, :TERM].each do |signal| trap(signal) do
             Print.warning('Received signal, killing node...')
             server.stop
           end
         end
       end
+    end
+
+    def self.tunnel_node
+      # TODO: Start ngrok tunneling
+    end
+
+    def self.register_node
+      # TODO: Register node remotelly
     end
 
   end
