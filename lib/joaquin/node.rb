8ad4ac4430eaff8e39522507db642515d53a9671
Carlos Vidal <nakioparkour@gmail.com>
2017-04-10 11:47:44 +0200

    node registration retry

--- lib/joaquin/node.rb
+++ lib/joaquin/node.rb
@@ -5,11 +5,16 @@ require 'json'
 module Joaquin
   class Node
 
-    @@server = nil
     @@queue = nil
     @@info = nil
 
     def self.call(request)
+      # Validate
+      if request['HTTP_JOAQUIN_SERVER_TOKEN'] != Joaquin.options.token
+        return [401, {'Content-Type' => 'application/json'}, [{error: 'Invalid server token'}.to_json]]
+      end
+
+      # Otherwise handle request
       case request['REQUEST_PATH']
       when SERVER_ENDPOINT_NODE_STATUS
         # TODO return node status
@@ -18,7 +23,7 @@ module Joaquin
       when SERVER_ENDPOINT_CANCEL_JOB
         # TODO cancel running job
       else
-        # TODO not found
+        return [404, {'Content-Type' => 'application/json'}, [{error: 'Resource could not be found'}.to_json]]
       end
       [200, {'Content-Type' => 'application/json'}, [request.to_json]]
     end
@@ -34,12 +39,13 @@ module Joaquin
       Print.warning("Booting up node at #{rack_options[:Host]}:#{rack_options[:Port]}...")
       Rack::Handler::WEBrick.run(Node, rack_options) do |server|
         Print.success("Node running at #{server.config[:BindAddress]}:#{server.config[:Port]}")
-        # Keep reference to server
-        Node.server = server
+
         # Create node info object
         Node.info = NodeInfo.new(server.config[:BindAddress], server.config[:Port])
+
         # Create jobs queue
         Node.queue = JobsQueue.new(Joaquin.options.concurrent_jobs)
+
         # Subscribe to kill signal
         [:INT, :TERM].each do |signal| trap(signal) do
             Print.warning('Received signal, killing node...')
@@ -47,6 +53,7 @@ module Joaquin
             server.stop
           end
         end
+
         # Register node remotelly
         Node.register_node
       end
@@ -61,12 +68,16 @@ module Joaquin
       Api.shared.register_node(Node.info) do |response|
         if respose.code == 201
           Print.success('Successfully registered node')
+
           # Start queue loop
           Node.queue.resume
         else
           Print.error("Unable to register node at host provided with address #{Joaquin.options.server_url.magenta}")
-          # Kill server
-          Node.server.stop
+
+          # Retry
+          Print.info('Retrying node registration in 10 seconds...')
+          sleep(10)
+          Node.register_node
         end
       end
     end
