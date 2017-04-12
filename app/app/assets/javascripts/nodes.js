992f7fd16868214f8cbb9541bd7b0e190d52ed9b
Carlos Vidal <nakioparkour@gmail.com>
2017-04-12 23:41:14 +0200

    node edition and destruction

--- app/app/assets/javascripts/nodes.js
+++ app/app/assets/javascripts/nodes.js
@@ -1,1 +1,7 @@
-
+var on_ready = function() {
+  $(".row-node").click(function() {
+    window.location = "/nodes/" + $(this).data("node-id");
+  });
+};
+$(document).ready(on_ready);
+$(document).on('page:load', on_ready);
--- app/app/controllers/nodes_controller.rb
+++ app/app/controllers/nodes_controller.rb
@@ -12,7 +12,7 @@ class NodesController < ApplicationController
     node = Node.new(node_params)
 
     if node.save
-      flash[:notice] = I18n.t('nodes.form.success_message', node_name: node.alias)
+      flash[:notice] = I18n.t('nodes.form.success_message_create', node_name: node.alias)
       redirect_to action: 'index'
     else
       flash[:error] = node.errors.full_messages.join('. ')
@@ -22,11 +22,36 @@ class NodesController < ApplicationController
 
   def new
     @node = Node.new({ alias: params[:alias], token: generate_hash })
+    @target_action = 'create'
   end
 
   def show
     @node = Node.find(params[:id])
+    @target_action = 'update'
     render 'new'
+  end
+
+  def update
+    node = Node.find(params[:id])
+    node.update_attributes(node_params)
+    if node.save
+      flash[:notice] = I18n.t('nodes.form.success_message_update', node_name: node.alias)
+      redirect_to action: 'index'
+    else
+      flash[:error] = node.errors.full_messages.join('. ')
+      redirect_to action: 'show', id: node.id
+    end
+  end
+
+  def destroy
+    node = Node.find(params[:id])
+    if node.destroy
+      flash[:notice] = I18n.t('nodes.form.success_message_delete', node_name: node.alias)
+      redirect_to action: 'index'
+    else
+      flash[:error] = node.errors.full_messages.join('. ')
+      redirect_to action: 'show', id: node.id
+    end
   end
 
   private
