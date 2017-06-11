02520759ffb1d0666624b613c005899619f3b43a
Carlos Vidal <nakioparkour@gmail.com>
2017-06-11 10:57:00 +0200

    loaded library and set generic theme

--- app/app/assets/javascripts/dependencies.js
+++ app/app/assets/javascripts/dependencies.js
@@ -6,3 +6,9 @@ window.Redux = require('redux/lib/index');
 React = require('react');
 ReactDOM = require('react-dom');
 PropTypes = require('prop-types');
+
+// UI
+MaterialUi = require('material-ui');
+injectTapEventPlugin = require('react-tap-event-plugin');
+injectTapEventPlugin();
+MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default;
